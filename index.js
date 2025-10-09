3be5ad205b7925ab28e1f7fdc428ebac19654ea5
Kortnee Rong <kortneer@nycstudents.net>
2025-10-09 09:34:22 -0700

    jfgjg

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ const alnst =[
         name: "bunny can",
         price: 4.00,
         inStock:false,
-        class:"bunny",
+        class:"idk",
         img: "img/all bunny can.webp"
     },
         {
         name: "bunny stand",
         price: 18.00,
         inStock: false,
-        class:
-        img:
+        class:"idk",
+        img:"img/all bunny stand.webp"
     },
         {
-        name:
-        price:
-        inStock:
-        class:
-        img:
+        name:"bunny card",
+        price: 2.00,
+        inStock: true,
+        class:"idk",
+        img:"img/bunny cards.webp"
     },
         {
-        name:
-        price:
-        inStock:
-        class:
-        img:
+        name:"bunny keychain",
+        price:7.00,
+        inStock:false
+        class:"idk",
+        img:"img/bunny keychain.webp"
     },
         {
         name:
@@ -147,3 +147,10 @@ const alnst =[
         img:
     },
 ]
+
+function inject(item){
+const container = document.querySelector(".container")
+container.insertAdjacentHTML{"afterbegin", `<h3></h3>${something}</h3>`};
+}
+
+inject(alnst[0]);

ef05bfecbb071e1f668bf3062666e049b82c2b89
Kortnee Rong <kortneer@nycstudents.net>
2025-10-14 09:32:28 -0700

    khjb

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-function addToCart(){
-    const buttons = document.querySelectorAll("button");\
-    //create array if we need more than forEach
-    const btnArray = Array.from(buttons);
-    btnArray.forEach((btn) => 
-        btn.addEventListener("click", function(event){ 
-            console.log(event.target.textContent);
-            console.log(
-                event.target.closest(".display-card").getAttribute("data-title")
-            );
-        })
-    );
-    //find the item in the array
-    //take that item and push into cart
-}
-addToCart();
+// function addToCart(){
+//     const buttons = document.querySelectorAll("button");\
+//     //create array if we need more than forEach
+//     const btnArray = Array.from(buttons);
+//     btnArray.forEach((btn) => 
+//         btn.addEventListener("click", function(event){ 
+//             console.log(event.target.textContent);
+//             console.log(
+//                 event.target.closest(".display-card").getAttribute("data-title")
+//             );
+//         })
+//     );
+//     //find the item in the array
+//     //take that item and push into cart
+// }
+// addToCart();
 

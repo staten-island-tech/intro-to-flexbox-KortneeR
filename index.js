9f3042a9953a62b1e12ca9f968aba45e719b1f7d
Kortnee Rong <kortneer@nycstudents.net>
2025-10-15 21:54:01 -0400

    i did something guys
    woo

--- index.js
+++ index.js
@@ -1,150 +1,130 @@
 const products =[
     {
-        name: "bunny can",
-        price: 4.00,
-        inStock:false,
-        class:"idk",
-        img: "img/all bunny can.webp"
+        name: "Free Stage Series- Blind Box Figure",
+        price: 11.00,
+        category: "Blind Boxes",
+        img: "alnst vol1 blindbox.webp",
+        alt: "Blind Box Figures"
     },
         {
-        name: "bunny stand",
+        name: "Free Stage Series- Plush Toy",
+        price: 4.20,
+        category: "Rabbit Series 1",
+        img: "bunny1-pin.webp",
+        alt: "Rabbit Series 1: Random Can Badges"
+    },
+        {
+        name: "Mini Doll Pendant Blind Box",
+        price: 9.00,
+        category: "Blind Boxes",
+        img: "alnst keychain.jpg",
+        alt: "Mini Doll Pendant Blind Box"
+    },   
+        {
+        name: "Sunny Doll Series",
+        price: 12.00,
+        category: "Blind Boxes",
+        img: "sunny-doll.webp",
+        alt: "Sunny Doll Series"
+    },    
+        {
+        name: "Baby Series- Mechanical Keycaps",
+        price: 7.00,
+        category: "Blind Boxes",
+        img: "keycap-chain.webp",
+        alt: "Baby Series- Mechanical Keycaps"
+    },    
+        {
+        name: "Baby Series- Figure Blind Box",
+        price: 10.00,
+        category: "Blind Boxes",
+        img: "baby box.webp",
+        alt: "Baby Series- Figure Blind Box"
+    },
+        {
+        name: "Blind Box Plush Doll",
+        price: 10.00,
+        category: "Blind Boxes",
+        img: "cutealnstbag.webp",
+        alt: "Blind Box Plush Doll"
+    },  
+        {
+        name: "Rabbit Series 1: Acrylic Stand",
         price: 18.00,
-        inStock: false,
-        class:"idk",
-        img:"img/all bunny stand.webp"
+        category: "Rabbit Series 1",
+        img: "all bunny stand.webp",
+        alt: "Rabbit Series 1: Acrylic Stand"
+    },  
+        {
+        name: "Rabbit Series 1: Rabbit Keyring",
+        price: 7.00,
+        category: "Rabbit Series 1",
+        img: "bunnykeyring.webp",
+        alt: "Rabbit Series 1: Rabbit Keyring"
+    },  
+        {
+        name: "Rabbit Series 1: Random Can Badges",
+        price: 4.20,
+        category: "Rabbit Series 1",
+        img: "bunny1-pin.webp",
+        alt: "Rabbit Series 1: Random Can Badges"
+    },  
+        {
+        name: "Rabbit Series 1: Rabbit Cushions",
+        price: 4.20,
+        category: "Rabbit Series 1",
+        img: "bunny-cushion.webp",
+        alt: "Rabbit Series 1: Rabbit Cushions"
+    },  
+        {
+        name: "Rabbit Series 1: Rabbit Mug",
+        price: 42.00,
+        category: "Rabbit Series 1",
+        img: "bunny mug.webp",
+        alt: "Rabbit Series 1: Rabbit Mug"
+    },  
+        {
+        name: "Rabbit Series 2: Mobile Phone Chain Blind Box",
+        price: 19.00,
+        category: "Rabbit Series 2",
+        img: "chains.webp",
+        alt: "Rabbit Series 2: Mobile Phone Chain Blind Box"
     },
         {
-        name:"bunny card",
-        price: 2.00,
-        inStock: true,
-        class:"idk",
-        img:"img/bunny cards.webp"
-    },
+        name: "Rabbit Series 2: Blind Box",
+        price: 12.00,
+        category: "Rabbit Series 2",
+        img: "mizi fat bunny.webp",
+        alt: "Rabbit Series 2: Blind Box"
+    }, 
         {
-        name:"bunny keychain",
-        price:7.00,
-        inStock:false,
-        class:"idk",
-        img:"img/bunny keychain.webp"
-    },
+        name: "Rabbit Series 2: Little Cute Carrot Blind Box",
+        price: 5.00,
+        category: "Rabbit Series 2",
+        img: "carrot-chain.webp",
+        alt: "Rabbit Series 2: Little Cute Carrot Blind Box"
+    },        
         {
-        name:"bunny mug",
-        price:42.00,
-        inStock:true,
-        class:"idk",
-        img:"img/bunny mug.webp"
-    },
+        name: "Rabbit Series 2: Random Can Badges",
+        price: 19.00,
+        category: "Rabbit Series 2",
+        img: "bunny-pin.webp",
+        alt: "Rabbit Series 2: Random Can Badges"
+    },         
         {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
+        name: "Rabbit Series 2: Card Set",
+        price: 10.00,
+        category: "Rabbit Series 2",
+        img: "bunny cards.webp",
+        alt: "Rabbit Series 2: Card Set"
+    },         
         {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-        {
-        name:
-        price:
-        inStock:
-        class:
-        img:
-    },
-]
+        name: "Card Collection Book",
+        price: 20.00,
+        category: "Rabbit Series 1",
+        img: "cardbook.webp",
+        alt: "Card Collection Book"
+    }, 
 
+
+]
